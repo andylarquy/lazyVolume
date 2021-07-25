f050f44f8a389a54e2085c95be0acfed9f5117f3
Andres Suarez <andres27059934@gmail.com>
2021-07-25 17:52:53 -0300

    Init frontend for host machine

---
+++ host/client-host/src/components/App/App.js
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Header from '../Header/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Content</h1>
+    </div>
+  )
+}
+
+export default App
